e061189a8c05f19f001e23d05f6b189df4210dc6
Sammy Cheruiyot <skipcheru@gmail.com>
2016-11-03 10:33:15 +0300

    Meaningful variable names

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,31 +1,24 @@
-def prime_number():
-    N = input("input start : ")
-    # modify this code later today to accept start and end of range of the prime numbers needed
-
-    for n in range(0, N):
-        if n > 1:
-            for i in range(2, n):
-                if n % i == 0:  # loop until the number is divisible by itself only
-                    break  # jump out of here
-            else:
-                print(n)
-
-  #  check if number is prime number
+def prime_numbers():
+    number = input("input range: ")
+    number_range = int(number)
+    for number in range(0, number_range):
+        if is_prime(number):
+            print(number)
 
 
-def is_prime():
-    N = input("Input a number : ")
-    n = int(N)
+def is_prime(number):
 
-    if n % 2 == 0 or n < 2:
+    if number % 2 == 0 and number is not 2:
         return False
-    if n == 2 or n == 3:
+    if number < 2:
+        return False
+    if number == 2 or number == 3:
         return True
 
-    for i in range(3, int(n**0.5) + 1, 2):   #check for numbers > 3 only here
-        if n % i == 0:
+    for i in range(3, int(number**0.5) + 1, 2):  # check for numbers > 3 only here
+        if number % i == 0:
             return False
 
     return True
 
-print(is_prime())
+prime_numbers()
